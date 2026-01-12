e42c84a157bbd6e241d8da01b53a5586feeae59d
Sean Alden <sa2596491@gmail.com>
2026-01-12 20:08:29 +0700

    Change api url

--- lib/constant/api_url.dart
+++ lib/constant/api_url.dart
@@ -1,3 +1,3 @@
-const String responseUrl = 'https://4ffea52a6acd.ngrok-free.app';
+// const String responseUrl = 'https://4ffea52a6acd.ngrok-free.app';
 // const String responseUrl = 'https://asiaraya.my.id'; 
-// const String responseUrl = 'https://backend-tugas-akhir-c14210052.vercel.app/api';
+const String responseUrl = 'https://backend-tugas-akhir-c14210052.vercel.app/api';
